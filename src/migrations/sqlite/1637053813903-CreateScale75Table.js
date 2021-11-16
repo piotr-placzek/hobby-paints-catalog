4c08afe6d962727a48069b104412e60ab8d9ed78
Piotr Płaczek <piotr.placzek.91@gmail.com>
2021-11-16 10:10:52 +0100

    #8 create migration for create scale75 paints table

---
+++ src/migrations/sqlite/1637053813903-CreateScale75Table.js
@@ -0,0 +1,24 @@
+// const { MigrationInterface, QueryRunner } = require("typeorm");
+
+module.exports = class CreateScale75Table1637053813903 {
+    async up(queryRunner) {
+        await queryRunner.query(
+            `
+            CREATE TABLE scale75_paints (
+                id INTEGER PRIMARY KEY,
+                catalog_number TEXT,
+                trade_name TEXT NOT NULL,
+                series TEXT,
+                image_url TEXT
+            );
+            `
+        );
+    }
+
+    async down(queryRunner) {
+        await queryRunner.query(
+            'DROP TABLE scale75_paints;'
+        );
+    }
+};
+        
