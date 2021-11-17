9052eaf8ae7713f80958febff78a23e2aabdb9ba
Piotr Płaczek <piotr.placzek.91@gmail.com>
2021-11-17 09:13:54 +0100

    #10 add gw catalog urls to env and config

--- src/shared/config.js
+++ src/shared/config.js
@@ -8,5 +8,15 @@ module.exports = {
         type: 'sqlite',
         database: 'database.sqlite',
         entities: ['src/entity/*.js']
-    }
+    },
+    CATALOG_URL: {
+        GW: {
+            BASE: process.env.CATALOG_URL_GW_BASE,
+            LAYER: process.env.CATALOG_URL_GW_LAYER,
+            AIR: process.env.CATALOG_URL_GW_AIR,
+            CONTRAST: process.env.CATALOG_URL_GW_CONTRAST,
+            DRY: process.env.CATALOG_URL_GW_DRY,
+            SHADE: process.env.CATALOG_URL_GW_SHADE,
+        }
+    } 
 };
