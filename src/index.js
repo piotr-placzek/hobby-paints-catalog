a519f00fa99a2215b205f731b3aff12219b1c9c9
Piotr Płaczek <piotr.placzek.91@gmail.com>
2021-11-24 18:45:36 +0100

    #10 update .gitignore README.md package.json src/index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,10 @@
 'use strict';
-
-const config = require('./shared/config');
-const DB = require('./shared/db');
-const { importPaintsCatalog } = require('./service/scrapingService');
-const gwScrapingStrategy = require('./strategy/gwScrapingStrategy');
-
-async function importGamesWorkshopPaintsCatalog(_db) {
-    console.time('importGamesWorkshopPaintsCatalog');
-    await importPaintsCatalog(Object.values(config.CATALOG_URL.GW), gwScrapingStrategy);
-    console.timeEnd('importGamesWorkshopPaintsCatalog');
-}
+const catalogImportService = require('./service/catalogImportService');
 
 async function main() {
-    const db = new DB(config.SQLITE);
-
     const scrapeGwFlag = process.argv.indexOf('--scrape-gw') >= 0;
     if (scrapeGwFlag) {
-        await importGamesWorkshopPaintsCatalog(db);
+        await catalogImportService.importGamesWorkshopPaintsCatalog();
     }
 }
 
