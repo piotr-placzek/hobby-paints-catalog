5ba6001b0d81231da5e67a2fe3defb1f2a5aac97
Piotr Płaczek <piotr.placzek.91@gmail.com>
2021-11-30 16:31:12 +0100

    #11 define va scraping strategy

--- src/strategy/vaScrapingStrategy.js
+++ src/strategy/vaScrapingStrategy.js
@@ -9,21 +9,32 @@ function vaScrapingStrategy(cheerio) {
     console.log('scraping strategy');
     const products = [];
     const selector = 'li.product.type-product>div.product-wrapper';
-    // cheerio(selector).each(
-    //     (_, item) => {
-    //         const imageUrl = cheerio(item).attr('src');
-    //         const product = productFactory(imageUrl);
-    //         products.push(product);
-    //     }
-    // );
+    cheerio(selector).each(
+        (_, li) => {
+            const item = cheerio.load(li);
+            const catalogNumber = item('.referencia').text();
+            const tradeName = item('.product-description h2').text();
+            const imageUrl = item('.product-images img').attr('data-src');
+            const productUrl = item('.product-description a').attr('href');
+            const series = extractSeries(productUrl);
+
+            products.push(db.VallejoPaint.build({
+                catalog_number: catalogNumber,
+                trade_name: tradeName,
+                series: series,
+                image_url: imageUrl
+            }));
+        }
+    );
     return products;
 }
 
 /**
- * @returns {VallejoPaint}
+ * @param {string} productUrl
+ * @returns {string}
  */
-function productFactory() {
-
+function extractSeries(productUrl) {
+    return productUrl.split('/')[5].split('-').slice(0,-1).join(' ');
 }
 
 module.exports = vaScrapingStrategy;
