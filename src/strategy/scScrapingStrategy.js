dcba3d23b4bae1458b27fc967cd827087004d9e1
Piotr Płaczek <piotr.placzek.91@gmail.com>
2021-12-01 13:05:21 +0100

    #13 add test for scale75 scraping strategy

---
+++ src/strategy/scScrapingStrategy.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * @param {cheerio} cheerio cheerio object with loaded html
+ * @returns {Scale75Paint[]}
+ */
+function scScrapingStrategy(cheerio) {
+    return [{}];
+}
+
+module.exports = scScrapingStrategy;
--- tests/strategy/scrapingStrategies.spec.js
+++ tests/strategy/scrapingStrategies.spec.js
@@ -3,6 +3,7 @@
 const cheerio = require("cheerio");
 const gwScrapingStrategy = require("../../src/strategy/gwScrapingStrategy");
 const vaScrapingStrategy = require("../../src/strategy/vaScrapingStrategy");
+const scScrapingStrategy = require("../../src/strategy/scScrapingStrategy");
 
 describe('Scraping strategies', () => {
     it('Games Workshop', async () => {
@@ -13,7 +14,7 @@ describe('Scraping strategies', () => {
             image_url: 'https://www.games-workshop.com/resources/catalog/product/600x620/99189950001_baseAverlandSunset.svg'
         };
         const htmlContainer =
-        `
+            `
         <div>
             <img class="test-img-prod4210269 record-spotlight__item-image" data-name="/Base-Averland-Sunset-2019" alt="Averland Sunset" src="https://www.games-workshop.com/resources/catalog/product/600x620/99189950001_baseAverlandSunset.svg"></img>
         </div>
@@ -75,6 +76,74 @@ describe('Scraping strategies', () => {
         const result = vaScrapingStrategy(C)[0];
 
         expect(result).toMatchObject(expected);
+    });
+
+    it('Scale75', async () => {
+        const expected = {
+            catalog_number: 'SC01',
+            trade_name: 'BLACK',
+            series: 'scalecolor',
+            image_url: 'https://scale75.com/1732-home_default/black.jpg'
+        };
+        const htmlContainer =
+            `
+        <li class="ajax_block_product first-in-line first-item-of-tablet-line first-item-of-mobile-line col-xs-12">
+        <div class="product-container">
+            <div class="row">
+                <div class="left-block col-xs-4 col-sm-5 col-md-4">
+                    <div class="product-image-container"> <a class="product_img_link"
+                            href="https://scale75.com/en/scalecolor-range/226-black.html" title="BLACK" itemprop="url"> <img
+                                class="replace-2x img-responsive" src="https://scale75.com/1732-home_default/black.jpg"
+                                alt="BLACK" title="BLACK" width="250" height="250" itemprop="image"> </a>
+                        <div class="content_price" itemprop="offers" itemscope="" itemtype="https://schema.org/Offer"> <span
+                                itemprop="price" class="price product-price"> 2,65 € </span>
+                            <meta itemprop="priceCurrency" content="EUR"> <span class="unvisible">
+                                <link itemprop="availability" href="https://schema.org/InStock">In Stock
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="center-block col-xs-4 col-sm-7 col-md-4">
+                    <div class="product-flags"></div>
+                    <h5 itemprop="name"> <a class="product-name"
+                            href="https://scale75.com/en/scalecolor-range/226-black.html" title="BLACK" itemprop="url">
+                            BLACK </a></h5>
+                    <p class="product-desc"> Acrylic paint 17ml</p>
+                    <div class="color-list-container"></div><span class="availability"> <span class=" label-success"> In
+                            Stock </span> </span>
+                </div>
+                <div class="right-block col-xs-4 col-sm-12 col-md-4">
+                    <div class="right-block-content row">
+                        <div class="content_price col-xs-5 col-md-12"> <span itemprop="price" class="price product-price">
+                                2,65 € </span>
+                            <meta itemprop="priceCurrency" content="EUR"> <span class="unvisible">
+                                <link itemprop="availability" href="https://schema.org/InStock">In Stock
+                            </span>
+                        </div>
+                        <div class="button-container col-xs-7 col-md-12"> <a
+                                class="button ajax_add_to_cart_button btn btn-default"
+                                href="https://scale75.com/en/cart?add=1&amp;id_product=226&amp;token=ddb2a3bd47fed5df27aedf46d6159be0"
+                                rel="nofollow" title="Add to cart" data-id-product-attribute="0" data-id-product="226"
+                                data-minimal_quantity="1"> <span>Add to cart</span> </a> <a
+                                class="button lnk_view btn btn-default"
+                                href="https://scale75.com/en/scalecolor-range/226-black.html" title="View">
+                                <span>More</span> </a></div>
+                        <div class="functional-buttons clearfix col-sm-12">
+                            <div class="compare"> <a class="add_to_compare"
+                                    href="https://scale75.com/en/scalecolor-range/226-black.html" data-id-product="226">Add
+                                    to Compare</a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </li>
+    `;
+
+    const C = await cheerio.load(htmlContainer);
+    const result = scScrapingStrategy(C)[0];
+
+    expect(result).toMatchObject(expected);
     });
 
 });
