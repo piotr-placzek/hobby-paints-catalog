c9f4f942f4e41bf58a097345cb126075b0e800e8
Piotr Płaczek <piotr.placzek.91@gmail.com>
2021-12-16 11:45:43 +0100

    #29 #31 add credentials message

---
+++ src/commands/credits.js
@@ -0,0 +1,20 @@
+'use strict';
+
+async function handler(argv) {
+    console.log(`HPC - Hobby Paints Catalog by pplaczek
+
+    The application is intended for hobby purposes only
+    and is based on the terms of the Creative Commons license.
+    Check the licence using <license> command,
+    or read LICENSE.md file.
+
+    Predefined replacement lists come from the following open sources:
+    - miniemporium.pl [EOL: 01.12.2021]
+    `);
+}
+
+module.exports = {
+    handler,
+    command: 'credits',
+    desc: 'print credentials',
+};
