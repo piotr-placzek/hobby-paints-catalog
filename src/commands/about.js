79f048156d4ae5caca2d3eaceed8a582113254cf
Piotr Płaczek <piotr.placzek.91@gmail.com>
2022-01-17 10:51:10 +0100

    #85 write docs (#91)
    * #84 add about command
    * #85 update README
    * #85 add docs section: useful links
    * #85 fix typo
    * #85 add docs section: installation from source code
    * #85 add 01-introduction.md
    * #85 add 02-requirements.md
    * #85 add info about database initialization
    * #85 add 41-usage-command-line.md
    * #85 update toc
    * #85 update links and translation
    * #85 update example
    * #84 update translation

--- src/commands/about.js
+++ src/commands/about.js
@@ -9,7 +9,7 @@ function handler(argv) {
         - How to contribute:    https://github.com/piotr-placzek/hobby-paints-catalog/blob/develop/.github/CONTRIBUTING.md
         - Code of Conduct:      https://github.com/piotr-placzek/hobby-paints-catalog/blob/develop/CODE_OF_CONDUCT.md
         - Releases:             https://github.com/piotr-placzek/hobby-paints-catalog/releases
-        - Development progess:  https://github.com/piotr-placzek/hobby-paints-catalog/projects/1
+        - Development progress: https://github.com/piotr-placzek/hobby-paints-catalog/projects/1
         - Bug report:           https://github.com/piotr-placzek/hobby-paints-catalog/issues/new?assignees=piotr-placzek&labels=bug&template=bug_report.md
         - Feature request:      https://github.com/piotr-placzek/hobby-paints-catalog/issues/new?assignees=piotr-placzek&labels=bug&template=feature_request.md
 
