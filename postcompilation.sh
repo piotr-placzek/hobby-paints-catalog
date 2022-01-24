bd45fd9c07c7590216cb45adad0df68d172cd19d
Piotr Płaczek <piotr.placzek.91@gmail.com>
2022-01-24 17:37:43 +0100

    #82 move pkg config to package.json, add assets sqlite3.node

--- postcompilation.sh
+++ postcompilation.sh
@@ -1,4 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
-cp .env.template bin/.env
-cp database_production.sqlite bin/
+echo "NODE_ENV=production" > bin/.env
+echo "LOGGER_SEPARATOR=\" - \"" >> bin/.env
+
+TARGETS=('linux' 'macos' 'win')
+for dest in "${TARGETS[@]}"; do
+        mkdir -p bin/${dest}
+        cp bin/.env bin/${dest}/
+        cp database_production.sqlite bin/${dest}/
+        cp assets/${dest}/* bin/${dest}
+done
+
+mv bin/hobby-paints-catalog-linux bin/linux/hpc
+mv bin/hobby-paints-catalog-macos bin/macos/hpc
+mv bin/hobby-paints-catalog-win.exe bin/win/hpc.exe
+
+tar cfJ bin/linux.tar.xz bin/linux
+tar cfJ bin/macos.tar.xz bin/macos
+zip bin/win.zip bin/win
