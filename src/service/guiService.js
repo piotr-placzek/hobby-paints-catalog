fe084c5352e3c1fa4ba0293c9021425b550a0800
Piotr Płaczek <piotr.placzek.91@gmail.com>
2022-02-12 20:57:08 +0100

    #69 prepare data for gui

--- src/service/guiService.js
+++ src/service/guiService.js
@@ -1,11 +1,22 @@
 'use strict';
 
+const LoggerService = require('./loggerService');
+const logger = new LoggerService('gui-service');
+
 /**
  * @param {*} event
  * @param {*} arg
  */
-function getAllProducts(event, arg) {
-    event.reply('getAllProducts', 'It works');
+async function getAllProducts(event) {
+    const db = require('../shared/db');
+    const modelsCount = db.models.length;
+    for(let i=0; i<modelsCount; i++) {
+        logger.info(`Waiting for db: ${db.models[i]}`);
+        const records = await db[db.models[i]].findAll();
+        logger.info(`Received ${records.length} ${db.models[i]}.`);
+        event.reply('getAllProducts', records.map( r => r.dataValues));
+    }
+    event.reply('allProductsSent');
 }
 
 module.exports = {
